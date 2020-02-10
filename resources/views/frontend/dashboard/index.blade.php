a3fa710a0d92141245ac181ec9ab30cec5d48dda
peekinternational <peekinternational@gmail.com>
2020-02-10 13:24:27 +0500

    nabeel

--- resources/views/frontend/dashboard/index.blade.php
+++ resources/views/frontend/dashboard/index.blade.php
@@ -46,13 +46,13 @@
                   <tbody>
                     <tr>
                       <td>Business Number</td>
-                      <td>+60-3-2935-9078</td>
+                      <td>{{ $userplan->choice_number}}</td>
                       <td>Virtual Receptionist</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
-              <a href="" class="btn btn-success">Manage Extension</a>
+              <a href="" class="btn btn-success">Manage Number</a>
 
             </div>
             <hr>
@@ -93,13 +93,58 @@
 
         </div>
         <div class="col-lg-6 col-md-6 app-view-mainCol">
-          <div class="cards">
-            <div class="header">
-              <h4 class="title">Billing Information</h4>
+           <div class="cards">
+            <div class="row">
+            	<div class="col-md-12">
+            		<div class="header">
+            		  <h4 class="title">Billing Information</h4>
+            		</div>
+            		<div class="content">
+            		  <div class="table-responsive">
+            		    <table class="table  table-bordered">
+            		      <thead>
+            		        <tr>
+            		          <th>Package Info</th>
+            		          <th>Card Owner</th>
+            		          <th>Payment</th>
+            		        </tr>
+            		      </thead>
+            		      <tbody>
+            		        <tr>
+            		          <td>{{ $userplan->name}}</td>
+            		          <td>{{ $userplan->f_name}}</td>
+            		          <td>$ {{ $userplan->cost}}</td>
+            		        </tr>
+            		      </tbody>
+            		    </table>
+            		  </div>
+            		</div>
+            	</div>
+            	<div class="col-md-8">
+            		<div class="header">
+            		  <h4 class="title">Card Info</h4>
+            		</div>
+            		<div class="content">
+            		  <div class="table-responsive">
+            		    <table class="table  table-bordered">
+            		      <thead>
+            		        <tr>
+            		          <th>Card No</th>
+            		          <th>Card Type</th>
+            		        </tr>
+            		      </thead>
+            		      <tbody>
+            		        <tr>
+            		          <td>*******{{ $userplan->card_last_four}}</td>
+            		          <td>{{ $userplan->card_brand}}</td>
+            		        </tr>
+            		      </tbody>
+            		    </table>
+            		  </div>
+            		</div>
+            	</div>
             </div>
-            <div class="content">
-              
-            </div>
+            <hr>
           </div>
         </div>
 
