c5374b785f795f9b1210d1fb0d43817dd964c8d1
peekinternational <peekinternational@gmail.com>
2020-02-11 18:57:37 +0500

    design changes on login page

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -3,9 +3,22 @@
 @endsection
 @section('content')
 <div class="container">
-	<div class="row justify-content-center">
-		<div class="col-md-5 col-xs-12">
-			<h3 class="text-uppercase mb-3 mt-5">Login</h3>
+	<div class="row justify-content-center mt-5 mb-5">
+		<div class="text-center w-100 mt-4">
+			<h2>NAUTILUS LOGO</h2>
+		</div>
+		<div class="col-md-4 col-xs-12 mt-5 mb-5">
+			<div class="px-3 my-3">
+				<a href="" class="btn btn-facebook btn-block facebook text-left mb-4 py-2 text-white"><i class="fab fa-facebook pr-2"></i> Find us on Facebook</a>
+				<a href="" class="btn btn-instagram btn-block facebook text-left mb-4 py-2 text-white"><i class="fab fa-instagram pr-2"></i> Find us on Instagram</a>
+				<a href="" class="btn btn-twitter btn-block facebook text-left mb-4 py-2 text-white"><i class="fab fa-twitter pr-2"></i> Find us on Twitter</a>
+				<a href="" class="btn btn-linkedin btn-block facebook text-left mb-4 py-2 text-white"><i class="fab fa-linkedin pr-2"></i> Find us on Linkdin</a>
+				<a href="" class="btn btn-youtube btn-block facebook text-left mb-4 py-2 text-white"><i class="fab fa-youtube pr-2"></i> Find us on Youtube</a>
+
+			</div>
+		</div>
+		<div class="col-md-5 col-xs-12 mt-5 mb-5 shadow rounded">
+			<h3 class="text-uppercase mb-3 mt-3 text-center">Nautilus Login</h3>
 		@if(Session::has('verify'))
                <div class="alert alert-success">
                   {{ Session::get('verify') }}
@@ -22,7 +35,7 @@
                   </button>
                </div>
                @endif
-			<div class="login-form">
+			<div class="login-form pb-5">
 				<form action="" method="post">
 					{{ csrf_field() }}
 				  <div class="form-group">
