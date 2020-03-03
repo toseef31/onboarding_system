25e59df780d87acce87545ccaf2fc298652d7003
peekinternational <peekinternational@gmail.com>
2020-03-03 12:07:10 +0500

    nabeelssssss

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -11,7 +11,7 @@ class SubscriptionController extends Controller
     public function create(Request $request, Plan $plan)
     {
         if($request->user()->subscribedToPlan($plan->stripe_plan, 'main')) {
-            return redirect()->route('user-portal')->with('success', 'You have already subscribed the plan');
+            return redirect()->route('user-portal/dashboard')->with('success', 'You have already subscribed the plan');
         }
         $plan = Plan::findOrFail($request->get('plan'));
         //dd($request->stripeToken);
@@ -19,6 +19,6 @@ class SubscriptionController extends Controller
             ->newSubscription('main', $plan->stripe_plan)
             ->create($request->stripeToken);
         DB::table('numbers')->where('num_id',$request->user()->choice_number)->update(['status'=>'1']);
-        return redirect('user-portal')->with('success', 'Your plan subscribed successfully');
+        return redirect('user-portal/dashboard')->with('success', 'Your plan subscribed successfully');
     }
 }
